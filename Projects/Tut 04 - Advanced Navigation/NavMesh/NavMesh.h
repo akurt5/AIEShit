796697447b709ac7e70b8697ddf564532c828ca5
akurt5 <anton.n.kurtz@gmail.com>
2014-03-31 16:18:34 +1100

    started advanced nav

---
+++ Projects/Tut 04 - Advanced Navigation/NavMesh/NavMesh.h
@@ -0,0 +1,51 @@
+#ifndef __NavMesh_H_
+#define __NavMesh_H_
+
+#include "Application.h"
+#include <glm/glm.hpp>
+
+#include <FBXFile.h>
+
+// Derived application class that wraps up all globals neatly
+class NavMesh : public Application
+{
+public:
+
+	NavMesh();
+	virtual ~NavMesh();
+
+protected:
+
+	virtual bool onCreate(int a_argc, char* a_argv[]);
+	virtual void onUpdate(float a_deltaTime);
+	virtual void onDraw();
+	virtual void onDestroy();
+
+	struct NavNode
+	{
+		glm::vec3 Position, Vertices[3];
+		NavNode *edgeTarget;
+	};
+
+	std::vector <NavNode*> m_Graph;
+
+	void BuildNavMesh(FBXMeshNode *a_Mesh, std::vector<NavNode*> &a_Graph);
+
+	void	createOpenGLBuffers(FBXFile* a_fbx);
+	void	cleanupOpenGLBuffers(FBXFile* a_fbx);
+
+	struct GLData
+	{
+		unsigned int	vao, vbo, ibo;
+	};
+
+	FBXFile*	m_sponza;
+	FBXFile*	m_navMesh;
+
+	unsigned int	m_shader;
+
+	glm::mat4	m_cameraMatrix;
+	glm::mat4	m_projectionMatrix;
+};
+
+#endif // __NavMesh_H_
